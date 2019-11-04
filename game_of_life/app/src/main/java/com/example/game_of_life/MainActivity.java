ac8821301eb27446e9fdabe95f6df7a1db4e3182
Tony Calarese <accalarese@gmail.com>
2019-11-04 07:16:06 -0500

    initial commit
    initial commit for the basic app

---
+++ game_of_life/app/src/main/java/com/example/game_of_life/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.game_of_life;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
