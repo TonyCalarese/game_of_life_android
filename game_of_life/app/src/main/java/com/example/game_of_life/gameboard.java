35d354336ea4b4862cff7b16cecfe705ead9dea6
Tony Calarese <accalarese@gmail.com>
2019-11-06 16:34:24 -0500

    big time update
    big update for getting the board to appear and the code to properly function well with tthe clicking. However the reset button is not functioning properly

--- game_of_life/app/src/main/java/com/example/game_of_life/gameboard.java
+++ game_of_life/app/src/main/java/com/example/game_of_life/gameboard.java
@@ -9,38 +9,61 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.appcompat.app.AppCompatActivity;
 
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
+
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.TextView;
 
 import java.util.List;
 
 
 
 public class gameboard extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     public static final String LIFE_ID = "GAMEOFLIFE";
     private RecyclerView mGameBoard;
-    private PlanetAdapter mAdapter;
-
+    private Button mReset;
+    private TileAdapter mAdapter;
+    private int mX_size= 20, mY_size = 20; // Making the 20 by 20 board
+    private int mSize = mX_size * mY_size;
+    private int[] mGrid = { //Must be a more efficient way to do this
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, //row1
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, //row2
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, //row3
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, //row4
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, //row5
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, //row6
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, //row7
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, //row8
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, //row9
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, //row10
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, //row11
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, //row12
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, //row13
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, //row14
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, //row15
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, //row16
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, //row17
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, //row18
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, //row19
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 //row20
+            };
     private OnFragmentInteractionListener mListener;
 
+    //private Handler mTimer;
     public gameboard() {
         // Required empty public constructor
     }
 
+    //mUpdateHandler = new Handler(new Handler.Callback()
+    //@Overridepublic )
+
     public static gameboard newInstance(String param1, String param2) {
         gameboard fragment = new gameboard();
         Bundle args = new Bundle();
-
         fragment.setArguments(args);
         return fragment;
     }
@@ -52,18 +75,25 @@ public class gameboard extends Fragment {
         setHasOptionsMenu(true);
     }
 
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-
-        View view = inflater.inflate(R.layout.fragment_gameboard, container, false);
-
+        View view = inflater.inflate(R.layout.game_board_fragment, container, false);
         mGameBoard = (RecyclerView) view.findViewById(R.id.gameboard_recycler_view);
-        mGameBoard.setLayoutManager(new LinearLayoutManager(getActivity()));
-
-
+        mGameBoard.setLayoutManager(new GridLayoutManager(getActivity(), mX_size));
+        mAdapter = new TileAdapter();
+        mGameBoard.setAdapter(mAdapter);
+        mReset = (Button) view.findViewById(R.id.reset_button);
+        mReset.setOnClickListener(new View.OnClickListener() {
+                                      @Override
+                                      public void onClick(View view) {
+                                          resetBoard();
+                                      }
+                                  }
+        );
         return view;
     }
 
@@ -81,27 +111,29 @@ public class gameboard extends Fragment {
     }
 
     public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
     }
 
-    private class TileHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
-        private Button mButton;
+    private class TileHolder extends RecyclerView.ViewHolder{
+        private Button mSquare;
+        private Button mReset;
         private int mPosition;
 
         public TileHolder(LayoutInflater inflater, ViewGroup container) {
             super(inflater.inflate(R.layout.tile, container, false));
-            mButton = (Button)itemView.findViewById(R.id.tile_button);
-            mButton.setOnClickListener(new View.OnClickListener() {
+            mSquare = (Button)itemView.findViewById(R.id.tile_button);
+            mSquare.setOnClickListener(new View.OnClickListener() {
                                            @Override
                                            public void onClick(View view) {
-                                               if (mTouchEnabled && (mGrid[mPosition] == 0)) {
-                                                   mGrid[mPosition] = mTurn; // set move
-                                                   mTurn = 3 - mTurn; // flip player
+                                               if(mGrid[mPosition] == 0) {
+                                                   mGrid[mPosition] = 1;
+                                               }else {
+                                                   mGrid[mPosition] = 0;
+                                               }
+
                                                    mAdapter.notifyItemChanged(mPosition); // reload ViewHolder
 
                                                }
-                                           }
                                        }
             );
         }
@@ -109,39 +141,40 @@ public class gameboard extends Fragment {
         public void bindPosition(int p) {
             mPosition = p;
         }
-        public void onClick(View view){
-            Intent intent = PlanetPagerActivity.newIntent(getActivity(), mPlanet.getId());
-            startActivity(intent);
-        }
-
     }
 
 
 
-    private class SquareAdapter extends RecyclerView.Adapter<SquareHolder> {
+    private class TileAdapter extends RecyclerView.Adapter<TileHolder> {
         @Override
-        public void onBindViewHolder(SquareHolder holder, int position) {
+        public void onBindViewHolder(TileHolder holder, int position) {
             // tell holder which place on grid it is representing
             holder.bindPosition(position);
             // actually change image displayed
-            if (mGrid[position] == X) {
-                holder.mButton.setBackgroundResource(R.drawable.x_sign);
-            } else if (mGrid[position] == O) {
-                holder.mButton.setBackgroundResource(R.drawable.o_sign);
+            if (mGrid[position] == 1) {
+                holder.mSquare.setBackgroundResource(R.drawable.star_icon);
             } else {
-                holder.mButton.setBackgroundResource(R.drawable.empty);
+                holder.mSquare.setBackgroundResource(R.drawable.tile_background);
             }
         }
 
         @Override
-        public SquareHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        public TileHolder onCreateViewHolder(ViewGroup parent, int viewType) {
             LayoutInflater inflater = LayoutInflater.from(getActivity());
-            return new SquareHolder(inflater, parent);
+            return new TileHolder(inflater, parent);
         }
 
         @Override
         public int getItemCount() {
-            return 9;
+            return mX_size * mY_size;
+        }
+    }
+
+
+
+    public void resetBoard() {
+        for(int i= 0; i <mSize; i ++){
+            mGrid[i] = 0;
         }
     }
 
