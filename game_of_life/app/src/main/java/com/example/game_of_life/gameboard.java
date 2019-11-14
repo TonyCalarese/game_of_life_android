8974369dd134d4a08d87ebb8e6720fade783834f
Tony Calarese <accalarese@gmail.com>
2019-11-14 16:24:41 -0500

    made a ton of progress

--- game_of_life/app/src/main/java/com/example/game_of_life/gameboard.java
+++ game_of_life/app/src/main/java/com/example/game_of_life/gameboard.java
@@ -3,7 +3,6 @@ package com.example.game_of_life;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.drawable.Drawable;
-import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
@@ -22,30 +21,36 @@ import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.os.Handler;
-import android.widget.Toast;
+import android.widget.TextView;
 
-import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 
 
 //Source for animation: https://developer.android.com/guide/topics/graphics/drawable-animation.html
 public class gameboard extends Fragment {
-    public static final String GRID_ID = "GAMEOFLIFEGRID";
-    public static final String file = "saveGrid.txt";
-    private RecyclerView mGameBoard;
-    private Button mResetButton, mStartButton, mCloneButton, mSaveButton, mOpenButton, mColorButton;
-    private TileAdapter mAdapter;
-    private int mX_size= 20, mY_size = 20; // Making the 20 by 20 board
-    private int mSize = mX_size * mY_size;
-    private int mColorIndex = 0;
-    private int[] mColors = {R.color.colorPrimary, R.color.colorAccent, R.color.red,
-    R.color.gold, R.color.skyBlue};
+    public static final String GRID_ID = "GAMEOFLIFEGRID", file = "saveGrid.txt";
+
+    //Handler Elements
+    //Boolean for the Handler for going to the next generation
     private boolean mStarted = false;
+    private Handler mNextGenHandler = new Handler();
+
+    //App Elements
+    private RecyclerView mGameBoard;
+    private Button mResetButton, mStartButton, mCloneButton, mSaveButton, mOpenButton, mColorButton,
+            m5SecButton, m1SecButton, m10SecButton;
+    private TileAdapter mAdapter;
+    private TextView mGenCounterLabel, mCurrentSpeedLabel;
+
+    //Integers
+    public int mX_size= 20, mY_size = 20, mSize = mX_size * mY_size, // Making the 20 by 20 board and size 400
+            mColorIndex = 0, mGenNumber = 0, mSpeed = 10000;
+    //Colors in an array
+    private int[] mColors = {R.color.colorPrimary, R.color.colorAccent, R.color.red, R.color.gold, R.color.skyBlue};
+    //Grid
     private int[] mGrid = {
             0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, //row1
             0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, //row2
@@ -68,13 +73,11 @@ public class gameboard extends Fragment {
             0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, //row19
             0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 //row20
             };
-    private OnFragmentInteractionListener mListener;
-    private Handler mNextGenHnadler = new Handler();
-    //private Handler mTimer;
-    public gameboard() {
-        // Required empty public constructor
+    //Declaring the Life CalculatorClass
+    life_generator life = new life_generator(mX_size, mY_size);
 
-    }
+    //Empty Constructor
+    public gameboard() { }
 
     //mUpdateHandler = new Handler(new Handler.Callback()
     //@Overridepublic )
@@ -90,7 +93,7 @@ public class gameboard extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setHasOptionsMenu(true);
+
     }
 
 
@@ -98,27 +101,34 @@ public class gameboard extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
 
+        // Getting the Intent for the clone
         int[] Grid = getActivity().getIntent().getIntArrayExtra(GRID_ID);
         if(Grid != null){
             mGrid = Grid;
         }
 
-
+        //Get View
         View view = inflater.inflate(R.layout.game_board_fragment, container, false);
+
+        //Get the GameBord and adapter
         mGameBoard = (RecyclerView) view.findViewById(R.id.gameboard_recycler_view);
         mGameBoard.setLayoutManager(new GridLayoutManager(getActivity(), mX_size));
-        mAdapter = new TileAdapter();
-        mGameBoard.setAdapter(mAdapter);
+        resetAdapter(); //Setting up the adapter is the same as my resetAdapter Function
+
+        //Labels
+        mGenCounterLabel = (TextView) view.findViewById((R.id.GenCounter));
+        mGenCounterLabel.setText("0"); //Default it to
+        mCurrentSpeedLabel = (TextView) view.findViewById((R.id.current_speed));
+        //Buttons
         mResetButton = (Button) view.findViewById(R.id.reset_button);
         mResetButton.setOnClickListener(new View.OnClickListener() {
                                       @Override
                                       public void onClick(View view) {
                                           ceaseFunctions();
+                                          resetGenCounter();
                                           resetBoard();
-                                          mAdapter = new TileAdapter();
-                                          mGameBoard.setAdapter(mAdapter);
+                                          resetAdapter();
                                       }
                                   }
         );
@@ -127,7 +137,7 @@ public class gameboard extends Fragment {
                                       @Override
                                       public void onClick(View view) {
                                           if(mStarted == false){
-                                              mNextGenHnadler.post(nextGen);
+                                              mNextGenHandler.post(nextGen);
                                               mStartButton.setText(R.string.stop);
                                               mStarted=true;
                                           } else {
@@ -181,21 +191,66 @@ public class gameboard extends Fragment {
                                             @Override
                                             public void onClick(View view) {
                                                 changeColors();
-                                                mAdapter = new TileAdapter();
-                                                mGameBoard.setAdapter(mAdapter);
+                                                resetAdapter();
                                             }
                                         }
         );
+
+
+        //Speed Adjusters
+        m10SecButton = (Button) view.findViewById(R.id.TenSec_button);
+        m10SecButton.setOnClickListener(new View.OnClickListener() {
+                                            @Override
+                                            public void onClick(View view) {
+                                                mSpeed = 10000; //Setting the speed to 10,000 millisecs
+                                            }
+                                        }
+        );
+        m5SecButton = (Button) view.findViewById(R.id.TenSec_button);
+        m5SecButton.setOnClickListener(new View.OnClickListener() {
+                                            @Override
+                                            public void onClick(View view) {
+                                                mSpeed = 5000; //Setting the speed to 10,000 millisecs
+                                            }
+                                        }
+        );
+        m1SecButton = (Button) view.findViewById(R.id.TenSec_button);
+        m1SecButton.setOnClickListener(new View.OnClickListener() {
+                                            @Override
+                                            public void onClick(View view) {
+                                                mSpeed = 1000; //Setting the speed to 10,000 millisecs
+                                            }
+                                        }
+        );
+
+
+
+        //Return
         return view;
     }
-
-
+    //This will set the counter back to 0
+    public void resetGenCounter()
+    {
+        mGenNumber = 0;
+    }
+    public void incCounter()
+    {
+        mGenNumber++;
+        mGenCounterLabel.setText(String.valueOf(mGenNumber));
+    }
     //ceased the current Handler if it is present
     public void ceaseFunctions()
     {
-        mNextGenHnadler.removeCallbacks(nextGen);
+        if(checkBoard()== true){
+            resetGenCounter(); //if the board is blank then reset the counter
+        }
+        mNextGenHandler.removeCallbacks(nextGen);
         mStartButton.setText(R.string.start);
         mStarted = false;
+    }
+    public void setSpeedLabel()
+    {
+        mCurrentSpeedLabel.setText(String.valueOf(mSpeed));
     }
     public void changeColors(){
         //Code Source: https://stackoverflow.com/questions/46714018/change-android-button-drawable-icon-color-programmatically
@@ -227,13 +282,15 @@ public class gameboard extends Fragment {
         public void run() {
             //Code Reference: https://stackoverflow.com/questions/37995564/what-is-the-way-to-make-an-infinite-loop-in-a-thread-android
             //Please give credit where it is due
-            if (checkBoard() == true) {
+            if (checkBoard() == true) { //if the board is empty then stop everything
                 ceaseFunctions();
+                //But we want to keep the generation number because the user might want to know what generation
+                //the cells died at
             } else {
-                nextGeneration();
-                mAdapter = new TileAdapter();
-                mGameBoard.setAdapter(mAdapter);
-                mNextGenHnadler.postDelayed(nextGen, 250);
+                mGrid = life.nextGeneration(mGrid);
+                resetAdapter();
+                mNextGenHandler.postDelayed(nextGen, mSpeed);
+                incCounter();
             }
         }
     };
@@ -244,15 +301,9 @@ public class gameboard extends Fragment {
         //outState.putBoolean(LIFE_ID, mBoardState);
     }
 
-    @Override
-    public void onResume() {
-        super.onResume();
-    }
-
-    public interface OnFragmentInteractionListener {
-        void onFragmentInteraction(Uri uri);
-    }
-
+    //Idividual spots on the recycler view are considered individualTiles, this
+    //Will set up the adapter to interact with each of the tiles
+    //Setting the alive tiles to a star and the dead ones to a square
     private class TileHolder extends RecyclerView.ViewHolder{
         private Button mSquare;
         private int mPosition;
@@ -285,9 +336,8 @@ public class gameboard extends Fragment {
             // actually change image displayed
             if (mGrid[position] == 1) {
                 holder.mSquare.setBackgroundResource(R.drawable.star_icon);
-                //Code Referenced from: https://stackoverflow.com/questions/1634252/how-to-make-a-smooth-image-rotation-in-android
-                //Other Source: https://www.youtube.com/watch?v=fqU4zc_XeX0
-               Animation spinner = AnimationUtils.loadAnimation(getActivity(), R.anim.rotate);
+                //Source Code:  https://stackoverflow.com/questions/14216798/how-to-create-imageview-pulse-effect-using-nine-old-androids-animation -
+               Animation spinner = AnimationUtils.loadAnimation(getActivity(), R.anim.pulse_animation);
                holder.mSquare.startAnimation(spinner);
             } else {
                 holder.mSquare.setBackgroundResource(R.drawable.tile_background);
@@ -305,7 +355,7 @@ public class gameboard extends Fragment {
         }
     }
 
-
+    //Reset the board back to all 0s
     public void resetBoard() {
         //go through the board and reset it to 0
         for(int i= 0; i <mSize; i ++){
@@ -313,62 +363,11 @@ public class gameboard extends Fragment {
         }
     }
 
-    public void nextGeneration() {
-        int[][] mGridCopy = new int[mX_size][mY_size];
-        int lifeCounter = 0, n = 0, X_index, Y_index;
-
-        //loading the array
-        for (int i = 0; i < mX_size; i++) {
-            for (int j = 0; j < mY_size; j++) {
-                mGridCopy[i][j] = mGrid[n];
-                n++; //Turning the 1d array into a 2d array
-            }
-        }
-
-        //Getting the neighbor life count and setting the array
-        n = 0; //Reset n
-        for (int i = 0; i < mX_size; i++) {
-            for (int j = 0; j < mY_size; j++) {
-                //upperleft, center, right
-                   lifeCounter = mGridCopy[(((i-1)+ mX_size) % mX_size)][(((j-1)+ mY_size) % mY_size)] +
-                           mGridCopy[(((i-1)+ mX_size) % mX_size)][j] +
-                           mGridCopy[(((i-1)+ mX_size) % mX_size)][(((j+1)+ mY_size) % mY_size)] +
-                //Left, right
-                           mGridCopy[i][(((j-1)+ mY_size) % mY_size)] +
-                           //mGridCopy[i][j] +
-                           mGridCopy[i][(((j+1)+ mY_size) % mY_size)] +
-                //Downleft, center, right
-                           mGridCopy[(((i+1)+ mX_size) % mX_size)][(((j-1)+ mY_size) % mY_size)] +
-                           mGridCopy[(((i+1)+ mX_size) % mX_size)][j] +
-                           mGridCopy[(((i+1)+ mX_size) % mX_size)][(((j+1)+ mY_size) % mY_size)];
-
-
-               /* Each generation, a living cell with two or three living neighbors stays alive.
-                  A cell with any other number of neighbors (less or more) dies.*/
-
-                if(mGridCopy[i][j] == 1){
-                    if(lifeCounter == 3 || lifeCounter == 2) {
-                        mGrid[n] = 1;
-                    }
-                    else
-                    {
-                        mGrid[n] = 0;
-                    }
-                }else{
-                    if(lifeCounter == 3) {
-                            mGrid[n] = 1;
-                    }
-                    else{
-                        mGrid[n] = 0;
-                    }
-
-                }
-                n++; //Update n to go to the next cell
-
-            } //end of j for loop
-        } //end of i for loop
+    //Func to reset the adapter
+    public void resetAdapter(){
+        mAdapter = new TileAdapter();
+        mGameBoard.setAdapter(mAdapter);
     }
-
     //Used to stop the handler if the board is already empty, that way we can interrupt the loop
     public Boolean checkBoard() {
 
---
+++ game_of_life/app/src/main/java/com/example/game_of_life/life_generator.java
@@ -0,0 +1,67 @@
+package com.example.game_of_life;
+
+public class life_generator {
+    public int mX_size, mY_size;
+
+    public life_generator(int X_size, int Y_size){
+        mX_size = X_size;
+        mY_size = Y_size;
+    }
+
+    public int[] nextGeneration(int[] mGrid) {
+        int[][] mGridCopy = new int[mX_size][mY_size];
+        int lifeCounter = 0, n = 0, X_index, Y_index;
+
+        //loading the array
+        for (int i = 0; i < mX_size; i++) {
+            for (int j = 0; j < mY_size; j++) {
+                mGridCopy[i][j] = mGrid[n];
+                n++; //Turning the 1d array into a 2d array
+            }
+        }
+
+        //Getting the neighbor life count and setting the array
+        n = 0; //Reset n
+        for (int i = 0; i < mX_size; i++) {
+            for (int j = 0; j < mY_size; j++) {
+                //upperleft, center, right
+                lifeCounter = mGridCopy[(((i-1)+ mX_size) % mX_size)][(((j-1)+ mY_size) % mY_size)] +
+                        mGridCopy[(((i-1)+ mX_size) % mX_size)][j] +
+                        mGridCopy[(((i-1)+ mX_size) % mX_size)][(((j+1)+ mY_size) % mY_size)] +
+                        //Left, right
+                        mGridCopy[i][(((j-1)+ mY_size) % mY_size)] +
+                        //mGridCopy[i][j] +
+                        mGridCopy[i][(((j+1)+ mY_size) % mY_size)] +
+                        //Downleft, center, right
+                        mGridCopy[(((i+1)+ mX_size) % mX_size)][(((j-1)+ mY_size) % mY_size)] +
+                        mGridCopy[(((i+1)+ mX_size) % mX_size)][j] +
+                        mGridCopy[(((i+1)+ mX_size) % mX_size)][(((j+1)+ mY_size) % mY_size)];
+
+
+               /* Each generation, a living cell with two or three living neighbors stays alive.
+                  A cell with any other number of neighbors (less or more) dies.*/
+
+                if(mGridCopy[i][j] == 1){
+                    if(lifeCounter == 3 || lifeCounter == 2) {
+                        mGrid[n] = 1;
+                    }
+                    else
+                    {
+                        mGrid[n] = 0;
+                    }
+                }else{
+                    if(lifeCounter == 3) {
+                        mGrid[n] = 1;
+                    }
+                    else{
+                        mGrid[n] = 0;
+                    }
+
+                }
+                n++; //Update n to go to the next cell
+
+            } //end of j for loop
+        } //end of i for loop
+        return mGrid;
+    }
+}
