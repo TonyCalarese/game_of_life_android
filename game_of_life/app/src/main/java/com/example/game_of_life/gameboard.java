55aff71cee36cd75263fd1129949553837a92038
Tony Calarese <accalarese@gmail.com>
2019-11-13 21:52:23 -0500

    added tweaks
    added a few tweaks to the code and i think that it will start working now

--- game_of_life/app/src/main/java/com/example/game_of_life/gameboard.java
+++ game_of_life/app/src/main/java/com/example/game_of_life/gameboard.java
@@ -12,12 +12,15 @@ import androidx.fragment.app.Fragment;
 
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import android.util.Log;
 import android.view.LayoutInflater;
 
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.os.Handler;
+import android.widget.Toast;
 
 import java.io.BufferedReader;
 import java.io.FileInputStream;
@@ -161,6 +164,7 @@ public class gameboard extends Fragment {
                     ois.readObject();
                     fis.close();
                     txt = ois.toString();
+                    Log.v(GRID_ID, txt);
 
                 }catch (Exception e) {
                     e.printStackTrace();
